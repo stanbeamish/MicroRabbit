6d267c819b096d335edc26b59633939cfa1217cd
Olrik van Dijk <ovd@gmx.net>
2021-04-02 18:23:13 +0200

    Initial Commit of Infrastructure dependency container

---
+++ MicroRabbit/MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
