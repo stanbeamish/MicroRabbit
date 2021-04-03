107237fcf83bc635e65b8ad531a128493a24b9c6
Olrik van Dijk <ovd@gmx.net>
2021-04-03 17:19:19 +0200

    Completed the Frontend as APS.NET MVC app

---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,36 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        // Transfer Service is like a proxy service
+
+        public readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:5001/api/Banking";
+            var transferContent = new StringContent(
+                content: JsonConvert.SerializeObject(transferDto),
+                encoding: Encoding.UTF8,
+                mediaType: "application/json"
+            );
+
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
